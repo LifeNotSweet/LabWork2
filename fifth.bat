bf1d6423069dccc800da061db39ffacb9231c76e
LifeNotSweet <168427330+LifeNotSweet@users.noreply.github.com>
2025-05-16 21:28:23 +0300

    Scripts

---
+++ fifth.bat
@@ -0,0 +1,28 @@
+md Gazhev
+cd Gazhev
+md Sergey
+cd Sergey
+md Sergeevich
+pause
+
+cd ..
+echo > 18032005.txt
+cd Sergey
+cd Sergeevich
+echo > pk12345.txt
+pause
+
+cd ..
+cd ..
+cd ..
+del Gazhev /S /Q /F
+pause
+
+cd Gazhev
+cd Sergey
+rd Sergeevich
+cd ..
+rd Sergey
+cd ..
+rd Gazhev
+pause
